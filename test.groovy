f499a807e570656cd2c7067aa6190af04f8885fd
taregh <thashe01@dcs.bbk.ac.uk>
2017-03-15 12:32:22 +0000

    test

---
+++ test.groovy
@@ -0,0 +1,9 @@
+print "Please enter a number: "
+String str = System.console().readLine()
+int num = Integer.parseInt(str)
+if ( num > 10){
+	println "The number is bigger than 10."
+	}
+else{
+	println "The number is less than 10."
+	}
